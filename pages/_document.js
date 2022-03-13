7f48b8656686c703bc033e82898ec5c93d4cbf0d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-13 20:27:01 +0100

    fixed crossOrigin typo

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v6.0.0/css/all.css"
           integrity="sha384-3B6NwesSXE7YJlcLI9RpRqGf2p/EgVH8BgoKTaUrmKNDkHPStTQ3EyoYjCGXaOTS"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <link
           rel="stylesheet"
@@ -26,12 +26,12 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </Head>
       <body>
