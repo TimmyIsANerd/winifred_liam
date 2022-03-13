cdeac85dff767496a685d2a95daf99ea5e1a2c3e
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-13 23:09:05 +0100

    added base url to api data

--- component/Features/Features-Api.js
+++ component/Features/Features-Api.js
@@ -1,39 +1,47 @@
+const baseUrl = "localhost:3000"
+
 const data = [
   {
     id: 1,
-    image: "https://img.icons8.com/ios/64/000000/menu.png",
+    image: "https://img.icons8.com/ios/50/000000/document-writer.png",
     title: "Ghost Writing",
     desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+    url: `${baseUrl}/#portfolio`,
   },
   {
     id: 2,
-    image: "https://img.icons8.com/ios/40/000000/open-book.png",
+    image: "https://img.icons8.com/ios/50/000000/upwork.png",
     title: "Upwork Freelancing",
     desc: " It uses a dictionary of over 200 Latin words, combined witha handful of model sentence.",
+    url: ``,
   },
   {
     id: 3,
-    image: "https://img.icons8.com/fluency-systems-regular/64/000000/retro-tv.png ",
-    title: "App Development",
+    image: "https://img.icons8.com/ios/50/000000/technical-support.png ",
+    title: "IT Support Specialist",
     desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+    url: "",
   },
   {
     id: 4,
-    image: "https://img.icons8.com/material-outlined/64/000000/topic--v1.png ",
-    title: "Mobile App",
-    desc: "There are many variations of passages of Lorem Ipsum	available, but the majority.",
+    image: "https://img.icons8.com/ios/50/000000/amazon-kindle.png",
+    title: "Indie Author",
+    desc: "Written and Published 3 Books on Amazon and Bambooks",
+    url: "https://www.amazon.com/Winifred-Liam/e/B094FJXCV5/ref=dp_byline_cont_pop_ebooks_1",
   },
   {
     id: 5,
-    image: "https://img.icons8.com/ios-filled/64/000000/wifi.png ",
-    title: "CEO Marketing",
+    image: "https://img.icons8.com/ios/50/000000/medium-new.png",
+    title: "Blogger",
     desc: "always free from repetition, injected humour, or non-characteristic words etc.",
+    url: "https://medium.com/@winifredliam",
   },
   {
     id: 6,
-    image: "https://img.icons8.com/glyph-neue/50/000000/polyline.png ",
-    title: "Personal Portfolio April",
+    image: "https://img.icons8.com/ios/50/000000/technical-support.png",
+    title: "Virtual Assistant",
     desc: " It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.",
+    url: `${baseUrl}/#portfolio`,
   },
-]
-export default data
+];
+export default data;
