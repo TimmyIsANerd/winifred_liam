1218280fbba03b964fbb23709daf8635debd332f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-13 20:30:29 +0100

    set async

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ export default function Document() {
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossOrigin="anonymous" defer="true"
+          crossOrigin="anonymous" async
         ></script>
       </Head>
       <body>
