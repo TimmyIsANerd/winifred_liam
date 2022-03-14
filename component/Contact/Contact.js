56d82e2a5c0251b7ccf1ac1285e1256108499ee7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-14 08:08:29 +0100

    fixed form alignment

--- component/Contact/Contact.js
+++ component/Contact/Contact.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react"
-import contact1 from "./contact1.png"
+import React, { useState } from "react";
+import contact1 from "./contact1.png";
+import Image from 'next/image'
 
 const Contact = () => {
   const [data, setData] = useState({
@@ -7,21 +8,22 @@ const Contact = () => {
     phone: "",
     email: "",
     subject: "",
-    message: "",})
+    message: "",
+  });
 
   const InputEvent = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setData((preVal) => {
       return {
         ...preVal,
         [name]: value,
-      }
-    })
-  }
+      };
+    });
+  };
 
   const formSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     alert(
       `My name is ${data.fullname}. 
 	My phone number is ${data.phone}. 
@@ -29,79 +31,133 @@ const Contact = () => {
 	My Subject on  ${data.subject}. 
 	Here is my message I want to say : ${data.message}. 
 	`
-    )
-  }
+    );
+  };
   return (
     <>
-      <section className='Contact' id='contact'>
-        <div className='container top'>
-          <div className='heading text-center'>
-            <h4>CONTACT</h4>
-            <h1>Contact With Me</h1>
-          </div>
+      <div className="Contact container top" id="contact">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="heading text-center">
+              <h4>CONTACT</h4>
+              <h1>Say Hello</h1>
+            </div>
 
-          <div className='content d_flex'>
-            <div className='left'>
-              <div className='box box_shodow'>
-                <div className='img'>
-                  <img src={contact1} alt='' />
+            <div className="container">
+              <div className="row">
+                <div className="col-6">
+                  <div className="box box_shodow p-2">
+                    <div className="details">
+                      <h1>Winifred Liam</h1>
+                      {/* <p>Chief Operating Officer</p> */}
+                      <p>
+                        I am available for freelance work. Connect with me via
+                        and call in to my account.
+                      </p>{" "}
+                      <br />
+                      <p>Phone: +01234567890</p>
+                      <p>Email: admin@example.com</p> <br />
+                      <span>FIND WITH ME</span>
+                      <div className="button button_container">
+                      <a
+                href="https://www.facebook.com/winnifred.liam"
+                target="_blank"
+                className="btn_shadown"
+              >
+                <button className="btn_shadow">
+                  <i className="fab fa-facebook-f"></i>
+                </button>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/winifred-liam/"
+                target="_blank"
+              >
+                <button className="btn_shadow">
+                  <i className="fab fa-instagram"></i>
+                </button>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/winifred-liam/"
+                target="_blank"
+              >
+                <button className="btn_shadow">
+                  <i className="fab fa-linkedin-in"></i>
+                </button>
+              </a>
+              <a href="https://medium.com/@winifredliam" target="_blank">
+                <button className="btn_shadow">
+                  <i className="fab fa-medium"></i>
+                </button>
+              </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className='details'>
-                  <h1>Nevine Acotanza</h1>
-                  <p>Chief Operating Officer</p>
-                  <p>I am available for freelance work. Connect with me via and call in to my account.</p> <br />
-                  <p>Phone: +01234567890</p>
-                  <p>Email: admin@example.com</p> <br />
-                  <span>FIND WITH ME</span>
-                  <div className='button f_flex'>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-facebook-f'></i>
-                    </button>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-instagram'></i>
-                    </button>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-twitter'></i>
-                    </button>
+
+                <div className="col-6">
+                  <div className="box_shodow p-2">
+                    <form onSubmit={formSubmit}>
+                      <div className="col">
+                        <div className="input">
+                          <span>YOUR NAME</span>
+                          <input
+                            type="text"
+                            name="fullname"
+                            value={data.fullname}
+                            onChange={InputEvent}
+                          />
+                        </div>
+                        <div className="input">
+                          <span>PHONE NUMBER </span>
+                          <input
+                            type="number"
+                            name="phone"
+                            value={data.phone}
+                            onChange={InputEvent}
+                          />
+                        </div>
+                      </div>
+                      <div className="input">
+                        <span>EMAIL </span>
+                        <input
+                          type="email"
+                          name="email"
+                          value={data.email}
+                          onChange={InputEvent}
+                        />
+                      </div>
+                      <div className="input">
+                        <span>SUBJECT </span>
+                        <input
+                          type="text"
+                          name="subject"
+                          value={data.subject}
+                          onChange={InputEvent}
+                        />
+                      </div>
+                      <div className="input">
+                        <span>YOUR MESSAGE </span>
+                        <textarea
+                          cols="30"
+                          rows="10"
+                          name="message"
+                          value={data.message}
+                          onChange={InputEvent}
+                        ></textarea>
+                      </div>
+                      <button className="btn_shadow">
+                        SEND MESSAGE <i className="fa fa-long-arrow-right"></i>
+                      </button>
+                    </form>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className='right box_shodow'>
-              <form onSubmit={formSubmit}>
-                <div className='f_flex'>
-                  <div className='input row'>
-                    <span>YOUR NAME</span>
-                    <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
-                  </div>
-                  <div className='input row'>
-                    <span>PHONE NUMBER </span>
-                    <input type='number' name='phone' value={data.phone} onChange={InputEvent} />
-                  </div>
-                </div>
-                <div className='input'>
-                  <span>EMAIL </span>
-                  <input type='email' name='email' value={data.email} onChange={InputEvent} />
-                </div>
-                <div className='input'>
-                  <span>SUBJECT </span>
-                  <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
-                </div>
-                <div className='input'>
-                  <span>YOUR MESSAGE </span>
-                  <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
-                </div>
-                <button className='btn_shadow'>
-                  SEND MESSAGE <i className='fa fa-long-arrow-right'></i>
-                </button>
-              </form>
-            </div>
           </div>
         </div>
-      </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
